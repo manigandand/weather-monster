64458dd58df8d3510e313208fcd6797dba29d46b
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2020-02-10 05:04:04 +0530

    store interface added

--- store/interface.go
+++ store/interface.go
@@ -2,4 +2,21 @@ package store
 
 // Store global store interface - provides db intercae methods
 // for diff entities
-type Store interface{}
+type Store interface {
+	City() Cities
+	Temperature() Temperatures
+	Forecast() Forecasts
+	Webhook() Webhooks
+}
+
+// Cities store interface expose the city db methods
+type Cities interface{}
+
+// Temperatures store interface expose the Temperatures db methods
+type Temperatures interface{}
+
+// Forecasts store interface expose the Forecasts db methods
+type Forecasts interface{}
+
+// Webhooks store interface expose the Webhooks db methods
+type Webhooks interface{}
--- store/store.go
+++ store/store.go
@@ -1,1 +1,60 @@
 package store
+
+import (
+	"log"
+	"weather-monster/config"
+
+	"github.com/jinzhu/gorm"
+	// gorm postgres connection
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var dbConn *gorm.DB
+
+func init() {
+	db, err := gorm.Open(config.DBDriver, config.DBDataSource)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbConn = db
+}
+
+// Conn struct holds the store connection
+type Conn struct {
+	DB              *gorm.DB
+	CityConn        Cities
+	TemperatureConn Temperatures
+	ForecastsConn   Forecasts
+	WebhooksConn    Webhooks
+}
+
+// NewStore inits new store connection
+func NewStore() *Conn {
+	conn := new(Conn)
+	conn.CityConn = NewCityStore(conn)
+	conn.TemperatureConn = NewTemperatureStore(conn)
+	conn.ForecastsConn = NewForecastStore(conn)
+	conn.WebhooksConn = NewWebhookStore(conn)
+
+	return conn
+}
+
+// City implements the store interface and it returns the Cities interface
+func (s *Conn) City() Cities {
+	return s.CityConn
+}
+
+// Temperature implements the store interface and it returns the Temperatures interface
+func (s *Conn) Temperature() Temperatures {
+	return s.TemperatureConn
+}
+
+// Forecast implements the store interface and it returns the Forecasts interface
+func (s *Conn) Forecast() Forecasts {
+	return s.ForecastsConn
+}
+
+// Webhook implements the store interface and it returns the Webhooks interface
+func (s *Conn) Webhook() Webhooks {
+	return s.WebhooksConn
+}

12c1daad90d38db2e88dcb547f43b21dd7e680b9
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2020-02-10 15:54:40 +0530

    post to all registered webhook on new temp record

--- api/v1/temperatures/temperature.go
+++ api/v1/temperatures/temperature.go
@@ -1,10 +1,14 @@
 package temperatures
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"time"
 	"weather-monster/pkg/errors"
 	"weather-monster/pkg/respond"
+	"weather-monster/pkg/trace"
+
 	"weather-monster/schema"
 	"weather-monster/utils"
 )
@@ -26,7 +30,9 @@ func saveCityTemperaturesHandler(w http.ResponseWriter, r *http.Request) *errors
 	if err := utils.Decode(r, &input); err != nil {
 		return errors.BadRequest(err.Error()).AddDebug(err)
 	}
-	if _, err := store.City().GetByID(input.CityID); err != nil {
+
+	city, err := store.City().GetByID(input.CityID)
+	if err != nil {
 		return err
 	}
 
@@ -35,7 +41,51 @@ func saveCityTemperaturesHandler(w http.ResponseWriter, r *http.Request) *errors
 	if err != nil {
 		return err
 	}
+	go postToWebhooks(city, temp)
 
 	respond.Created(w, temp)
 	return nil
+}
+
+func postToWebhooks(city *schema.City, temp *schema.Temperature) {
+	webhooks, err := store.Webhook().GetByCityID(temp.CityID)
+	if err != nil {
+		return
+	}
+
+	data := map[string]interface{}{
+		"city_id": temp.CityID,
+		"city": map[string]interface{}{
+			"name":      city.Name,
+			"latitude":  city.Latitude,
+			"longitude": city.Longitude,
+		},
+		"min":       temp.Min,
+		"max":       temp.Max,
+		"timestamp": temp.Timestamp,
+	}
+
+	body := bytes.NewBuffer(nil)
+	if err := json.NewEncoder(body).Encode(data); err != nil {
+		trace.Log.Info(err)
+		return
+	}
+
+	for _, hook := range webhooks {
+		go func(url string, body *bytes.Buffer) {
+			trace.Log.Info("posting to > ", url)
+			req, err := http.NewRequest(http.MethodPost, url, body)
+			if err != nil {
+				return
+			}
+			req.Header.Set("Content-Type", "application/json")
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				return
+			}
+
+			defer resp.Body.Close()
+			trace.Log.Info(url, " > ", resp.StatusCode)
+		}(hook.CallbackURL, body)
+	}
 }
